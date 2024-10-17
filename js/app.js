7ea57dc845932e52259ef5e22a46f12d698dd04a
Luan Ferreira <178341285+luan-42@users.noreply.github.com>
2024-10-17 16:36:13 -0300

    :sparkles: Adicionar carrossel com swiper
    - Refatorar nome dos videos
    - Remover links
    - Adicionar app.js

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: false,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    on: {
+    slideChange: function () {
+            let videos = document.querySelectorAll('video');
+            Array.prototype.forEach.call(videos, function(video){ video.pause()});
+        }
+    },
+});
+
+document.getElementById("saveSub").onclick = () => {window.location.href = "#uso"; swiper.slideTo(0)};
+document.getElementById("copySub").onclick = () => {window.location.href = "#uso"; swiper.slideTo(1)};
+document.getElementById("sliceMp3").onclick = () => {window.location.href = "#uso"; swiper.slideTo(2)};
+
